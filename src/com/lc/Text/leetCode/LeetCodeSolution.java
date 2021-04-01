701a5e19a8c5dcd5363c268421f507efdd12fded
a1186873082 <1186873082@qq.com>
2021-04-01 20:42:31 +0800

    笨阶乘算法

--- src/com/lc/Text/leetCode/LeetCodeSolution.java
+++ src/com/lc/Text/leetCode/LeetCodeSolution.java
@@ -3153,7 +3153,7 @@ public class LeetCodeSolution {
      * @return
      */
     public ListNode rotateRight(ListNode head, int k) {
-        if(k == 0|| head == null || head.next == null){
+        if (k == 0 || head == null || head.next == null) {
             return head;
         }
         ListNode head1 = head;
@@ -3167,7 +3167,7 @@ public class LeetCodeSolution {
             return head1;
         }
         head.next = head1;
-        while(--add > 0){
+        while (--add > 0) {
             head1 = head1.next;
         }
         ListNode ret = head1.next;
@@ -3175,16 +3175,86 @@ public class LeetCodeSolution {
         return ret;
     }
 
+    /**
+     * 通常，正整数 n 的阶乘是所有小于或等于 n 的正整数的乘积。例如，factorial(10) = 10 * 9 * 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1。
+     * <p>
+     * 相反，我们设计了一个笨阶乘 clumsy：在整数的递减序列中，我们以一个固定顺序的操作符序列来依次替换原有的乘法操作符：乘法(*)，除法(/)，加法(+)和减法(-)。
+     * <p>
+     * 例如，clumsy(10) = 10 * 9 / 8 + 7 - 6 * 5 / 4 + 3 - 2 * 1。然而，这些运算仍然使用通常的算术运算顺序：我们在任何加、减步骤之前执行所有的乘法和除法步骤，并且按从左到右处理乘法和除法步骤。
+     * <p>
+     * 另外，我们使用的除法是地板除法（floor division），所以 10 * 9 / 8 等于 11。这保证结果是一个整数。
+     * <p>
+     * 实现上面定义的笨函数：给定一个整数 N，它返回 N 的笨阶乘。
+     * <p>
+     * 解法一： 暴力解法， 用一个数存储乘法和除法的结果，然后进行加减
+     *
+     * @param N
+     * @return
+     */
+    public int clumsy(int N) {
+        int[] fuhao = new int[4];
+        for (int i = 0; i < fuhao.length; i++) {
+            fuhao[i] = (N - i) % 4;
+        }
+        //乘法除法需要先算
+        int multiAndDivide = 0;
+        int count = 0;
+        for (int i = N; i > 0; --i) {
+            if (i % 4 == fuhao[0]) {
+                if (i != N) {
+                    multiAndDivide = -i;
+                } else {
+                    multiAndDivide = i;
+                }
+            } else if (i % 4 == fuhao[1]) {
+                multiAndDivide *= i;
+            } else if (i % 4 == fuhao[3]) {
+                count += i;
+            } else {
+                multiAndDivide /= i;
+                count += multiAndDivide;
+                multiAndDivide = 0;
+            }
+        }
+        count += multiAndDivide;
+        double floor = Math.floor(count);
+        return (int) floor;
+    }
+
+    /**
+     * 解法二， 利用栈来解决
+     *
+     * @param N
+     * @return
+     */
+    public int clumsy1(int N) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(N);
+        N--;
+        int index = 0;
+        while (N > 0) {
+            if (index % 4 == 0) {
+                stack.push(stack.pop() * N);
+            } else if (index % 4 == 1) {
+                stack.push(stack.pop() / N);
+            } else if (index % 4 == 2) {
+                stack.push(N);
+            } else {
+                stack.push(-N);
+            }
+            index++;
+            N--;
+        }
+        int count = 0;
+        for (Integer integer : stack) {
+            count += integer;
+        }
+        return count;
+    }
+
     public static void main(String[] args) {
-//        ListNode head = new LeetCodeSolution().new ListNode(1);
-//        ListNode head1 = head;
-//
-//        for (int i = 2; i <= 5; i++) {
-//            head.next = new LeetCodeSolution().new ListNode(i);
-//            head = head.next;
-//        }
-//        System.out.println(new LeetCodeSolution().rotateRight(head1, 2));
-        String string = new String("123");
+        int clumsy = new LeetCodeSolution().clumsy1(10);
+        System.out.println(clumsy);
     }
 
     public static Date getPastDate(int past, Date fromDate) {
